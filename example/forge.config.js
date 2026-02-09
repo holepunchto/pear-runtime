b9f35ca048584a494fd007fa81e1bca531817e55
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-09 13:07:32 +0100

    Disable fuses

--- example/forge.config.js
+++ example/forge.config.js
@@ -1,9 +1,5 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses')
-const { FuseV1Options, FuseVersion } = require('@electron/fuses')
-
 module.exports = {
   packagerConfig: {},
-  rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-zip',
@@ -17,16 +13,5 @@ module.exports = {
       name: '@electron-forge/maker-rpm',
       config: {}
     }
-  ],
-  plugins: [
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true
-    })
   ]
 }
