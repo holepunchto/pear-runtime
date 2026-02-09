da564a425561dcad5f2764047cdc30521a6d3776
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-09 12:55:34 +0100

    Disable ASAR packaging

--- example/forge.config.js
+++ example/forge.config.js
@@ -2,9 +2,7 @@ const { FusesPlugin } = require('@electron-forge/plugin-fuses')
 const { FuseV1Options, FuseVersion } = require('@electron/fuses')
 
 module.exports = {
-  packagerConfig: {
-    asar: true
-  },
+  packagerConfig: {},
   rebuildConfig: {},
   makers: [
     {
@@ -21,10 +19,6 @@ module.exports = {
     }
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {}
-    },
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
