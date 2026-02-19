ce042df2aa4c7f9a1bd82683995f97b900bed8b8
Geordan Gesink <georgegeordan@gmail.com>
2026-02-19 16:44:06 +0100

    extend ready-resource; edit readme

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const PRU = require('pear-runtime-updater')
+const ReadyResource = require('ready-resource')
 const Sidecar = require('bare-sidecar')
 
-module.exports = class PearRuntime {
+module.exports = class PearRuntime extends ReadyResource {
   constructor(opts = {}) {
-    this.updater = opts.updates ? new PRU(opts) : null
+    if (!opts.dir) throw new Error('dir required')
+
+    this.dir = opts.dir
+    this.version = opts.version || 0
+    this.storage = opts.storage || path.join(this.dir, 'app-storage')
+
+    this.updater = opts.updates !== false ? new PRU(opts) : null
+
+    this.ready().catch(noop)
+  }
+
+  async _open() {
+    await this.updater?.ready()
+  }
+
+  async _close() {
+    // TODO: close any sidecars
+    await this.updater?.close()
   }
 
   run(entrypoint, args = [], opts = {}) {
     return new Sidecar(entrypoint, args, opts)
   }
 }
+
+function noop() {}
