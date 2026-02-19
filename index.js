f63aaf83e3e1c3f4ab72d961f602e77c260d4c2c
Geordan Gesink <georgegeordan@gmail.com>
2026-02-19 15:47:32 +0100

    updater on updater prop

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const PearRuntimeUpdater = require('pear-runtime-updater')
+const PRU = require('pear-runtime-updater')
 const Sidecar = require('bare-sidecar')
 
-module.exports = class PearRuntime extends PearRuntimeUpdater {
-  constructor(config) {
-    super(config)
+module.exports = class PearRuntime {
+  constructor(opts = {}) {
+    this.updater = opts.updates ? new PRU(opts) : null
   }
 
   run(entrypoint, args = [], opts = {}) {
