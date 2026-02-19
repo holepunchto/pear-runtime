325d1f73801999bce201a0353cdc6ee10762cbd1
Geordan Gesink <georgegeordan@gmail.com>
2026-02-19 15:28:01 +0100

    move apllyUpdate

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-const path = require('path')
-const fs = require('fs')
-const fsx = require('fs-native-extensions')
 const PearRuntimeUpdater = require('pear-runtime-updater')
 const Sidecar = require('bare-sidecar')
-const { platform, arch } = require('which-runtime')
-const host = platform + '-' + arch
 
 module.exports = class PearRuntime extends PearRuntimeUpdater {
   constructor(config) {
     super(config)
-    if (!config.dir) throw new Error('dir required')
-
-    if (this.dir === undefined) this.dir = config.dir
-    this.storage = path.join(this.dir, 'app-storage')
-    this.app = config.app
-    this.name = this.app && path.basename(this.app)
-    this.bundled = config.bundled || !!this.app
   }
 
   run(entrypoint, args = [], opts = {}) {
     return new Sidecar(entrypoint, args, opts)
-  }
-
-  async applyUpdate() {
-    if (!this.updated || this.applied || !this.bundled) return
-    this.applied = true
-
-    // mac only for now, linux similar, windows, more pain
-    await fsx.swap(path.join(this.next, 'by-arch', host, 'app', this.name), this.app)
-    await fs.promises.rm(this.next, { recursive: true, force: true })
   }
 }
