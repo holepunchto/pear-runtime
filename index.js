f5af21b696f4b340044421390964d2aa8f6003f1
Geordan Gesink <georgegeordan@gmail.com>
2026-03-09 22:53:33 +0100

    format

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ module.exports = class PearRuntime extends ReadyResouce {
     this.dir = opts.dir
     this.storage = opts.storage || path.join(this.dir, 'app-storage')
     this.updater = new PearRuntimeUpdater(opts)
-    this.updater.on('error', (err) => {
-      this.emit('error', err)
-    })
+    this.updater.on('error', (err) => this.emit('error', err))
   }
 
   async _open() {
