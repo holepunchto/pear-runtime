26e7c61885784ff0b2b840b9936fc70bbf2bed28
Geordan Gesink <georgegeordan@gmail.com>
2026-06-11 22:43:40 +0200

    add argv test; adjust tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,13 @@
 const PearRuntime = require('..')
 const test = require('brittle')
 const path = require('path')
+const { isBare } = require('which-runtime')
 
 test('worker echo', (t) => {
   t.plan(2)
 
-  const worker = PearRuntime.run(path.join(__dirname, 'fixture', 'worker.js'))
+  const specifier = require.resolve('./fixture/echo.js')
+  const worker = PearRuntime.run(specifier)
 
   const s = 'hello world'
 
@@ -19,3 +21,30 @@ test('worker echo', (t) => {
 
   worker.write(s)
 })
+
+test('worker argvs', async (t) => {
+  t.plan(1)
+
+  const specifier = require.resolve('./fixture/argv.js')
+  const worker = PearRuntime.run(specifier, ['test'])
+  t.teardown(() => worker.destroy())
+
+  const message = once(worker, 'data')
+  worker.write('ping')
+
+  const argv = JSON.parse((await message).toString())
+
+  t.alike(argv, [expectedArgv0(), specifier, 'test'])
+})
+
+function expectedArgv0() {
+  if (isBare) return require('bare-os').execPath()
+  return require(path.join(path.dirname(require.resolve('bare-sidecar')), 'lib', 'bare'))
+}
+
+function once(emitter, event) {
+  return new Promise((resolve, reject) => {
+    emitter.once(event, resolve)
+    emitter.once('error', reject)
+  })
+}
